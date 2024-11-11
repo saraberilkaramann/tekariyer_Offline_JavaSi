3ad7e0e89cf544f58604d3bb832028e16966df38
Sara <berilkaraman39@gmail.com>
2024-11-11 19:07:15 +0300

    hello

---
+++ src/main/java/org/example/_helloworld.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.function.DoubleToIntFunction;
+
+public class _helloworld {
+    public static void main(String[] args) {
+        System.out.println("merhaba sara Java SE");
+    }
+}
